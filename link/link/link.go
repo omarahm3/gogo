acdea66eda1097a15a37c4d988ca694c6fdacda8
Omar Ahmed <omarking05@gmail.com>
2022-08-08 15:34:42 +0200

    format code

--- link/link/link.go
+++ link/link/link.go
@@ -12,19 +12,19 @@ type Link struct {
 }
 
 func extractText(n *html.Node) (ret string) {
-  if n.Type == html.TextNode {
-    return n.Data
-  }
+	if n.Type == html.TextNode {
+		return n.Data
+	}
 
-  if n.Type != html.ElementNode {
-    return ""
-  }
+	if n.Type != html.ElementNode {
+		return ""
+	}
 
-  for c := n.FirstChild; c != nil; c = c.NextSibling {
-    ret += extractText(c) + " "
-  }
-  
-  return strings.Join(strings.Fields(ret), " ")
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		ret += extractText(c) + " "
+	}
+
+	return strings.Join(strings.Fields(ret), " ")
 }
 
 func Parse(n *html.Node) []Link {
@@ -41,7 +41,7 @@ func Parse(n *html.Node) []Link {
 				}
 			}
 
-      text := extractText(n)
+			text := extractText(n)
 
 			links = append(links, Link{
 				Href: href,
